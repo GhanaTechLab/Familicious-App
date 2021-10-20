620fad257e79be0105efacb39f7762918ffb0fae
Etornam <sunumacbright@gmail.com>
2021-10-20 22:39:26 +0000

    fix error with file upload service

--- lib/services/file_upload_service.dart
+++ lib/services/file_upload_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 
 class FileUploadService {
   final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
@@ -8,14 +9,18 @@ class FileUploadService {
   Future<String?> uploadFile(
       {required File file, required String userUid}) async {
     try {
-      UploadTask storageUploadTask = _firebaseStorage
-          .ref()
-          .child('profile_images')
-          .child('$userUid.jpg')
-          .putFile(file);
-      return storageUploadTask.snapshot.ref.getDownloadURL();
-    } on FirebaseException catch (e) {
-      print(e.message);
+    
+      Reference storageRef =
+          _firebaseStorage.ref().child('profile_images').child('$userUid.jpg');
+
+      UploadTask storageUploadTask = storageRef.putFile(file);
+
+      TaskSnapshot snapshot = await storageUploadTask.whenComplete(()=> storageRef.getDownloadURL());
+
+      return await snapshot.ref.getDownloadURL();
+
+    } catch (e) {
+      debugPrint('############ $e');
       return null;
     }
   }
