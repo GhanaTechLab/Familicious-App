0c12fec5e7ad1fe6732af1bb722c476ab72c92df
Etornam <sunumacbright@gmail.com>
2021-10-20 22:39:09 +0000

    finish up auth integration

--- lib/managers/auth_manager.dart
+++ lib/managers/auth_manager.dart
@@ -2,34 +2,41 @@ import 'dart:io';
 
 import 'package:famlicious_app/services/file_upload_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AuthManager with ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  static final FirebaseFirestore _firebaseFirestore =
+      FirebaseFirestore.instance;
   final FileUploadService _fileUploadService = FileUploadService();
   String _message = 'hello';
   bool _isLoading = false;
+  CollectionReference userCollection = _firebaseFirestore.collection("users");
 
-  String get message => _message; //get
-  bool get isLoading => _isLoading;
+  String get message => _message; //getter
+  bool get isLoading => _isLoading; //getter
 
   setMessage(String message) {
-    //setting
+    //setter
     _message = message;
     notifyListeners();
   }
 
   setIsLoading(bool loading) {
+    //setter
     _isLoading = loading;
     notifyListeners();
   }
 
   //email, password
-  createNewUser(
+  Future<bool> createNewUser(
       {required String name,
       required String email,
       required String password,
       required File imageFile}) async {
+    setIsLoading(true);
+    bool isCreated = false;
     await _firebaseAuth
         .createUserWithEmailAndPassword(email: email, password: password)
         .then((userCredential) async {
@@ -37,16 +44,28 @@ class AuthManager with ChangeNotifier {
           file: imageFile, userUid: userCredential.user!.uid);
 
       if (photoUrl != null) {
-        // add user info to firestore (name,email,photo,uid,createdAt)
-
-        
+        await userCollection.doc(userCredential.user!.uid).set({
+          "name": name,
+          "email": email,
+          "picture": photoUrl,
+          "createdAt": FieldValue.serverTimestamp(),
+          "user_id": userCredential.user!.uid
+        });
+        isCreated = true;
       } else {
         setMessage('Image upload failed!');
+        isCreated = false;
       }
+      setIsLoading(false); //set to false
     }).catchError((onError) {
       setMessage('$onError');
+      isCreated = false;
+      setIsLoading(false); //set to false
     }).timeout(const Duration(seconds: 60), onTimeout: () {
       setMessage('Please check your internet connection.');
+      isCreated = false;
+      setIsLoading(false); //set to false
     });
+    return isCreated;
   }
 }
