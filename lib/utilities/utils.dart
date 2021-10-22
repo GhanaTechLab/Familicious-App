2c7229f6c118fbf12c1980f5fd270bfc7caa7250
Etornam <sunumacbright@gmail.com>
2021-10-22 13:04:26 +0000

    Create utils.dart

---
+++ lib/utilities/utils.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+Future<File?> myImageCropper(String filePath) async{
+return await ImageCropper.cropImage(
+        sourcePath: filePath,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: const AndroidUiSettings(
+            toolbarTitle: 'My Cropper',
+            toolbarColor: Colors.black,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: const IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ));
+}
