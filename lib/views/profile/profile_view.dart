1d3c1d59422251040c15b36861ee9a12bb36dcd3
Etornam <sunumacbright@gmail.com>
2021-10-22 13:04:39 +0000

    Update profile_view.dart

--- lib/views/profile/profile_view.dart
+++ lib/views/profile/profile_view.dart
@@ -1,12 +1,28 @@
+import 'package:famlicious_app/views/auth/login_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
 
 class ProfileView extends StatelessWidget {
-  const ProfileView({ Key? key }) : super(key: key);
+  const ProfileView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          TextButton.icon(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut().then((value) =>
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (_) => const LoginView()),
+                        (route) => false));
+              },
+              icon: const Icon(UniconsLine.exit),
+              label: const Text('Logout'))
+        ],
+      ),
     );
   }
 }
