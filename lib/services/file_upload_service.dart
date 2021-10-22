f927f0be1df7717968c20ed0fab0a2b6b08c35c4
Etornam <sunumacbright@gmail.com>
2021-10-22 13:04:24 +0000

    Update file_upload_service.dart

--- lib/services/file_upload_service.dart
+++ lib/services/file_upload_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class FileUploadService {
   final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
@@ -9,16 +10,34 @@ class FileUploadService {
   Future<String?> uploadFile(
       {required File file, required String userUid}) async {
     try {
-    
       Reference storageRef =
           _firebaseStorage.ref().child('profile_images').child('$userUid.jpg');
 
       UploadTask storageUploadTask = storageRef.putFile(file);
 
-      TaskSnapshot snapshot = await storageUploadTask.whenComplete(()=> storageRef.getDownloadURL());
+      TaskSnapshot snapshot = await storageUploadTask
+          .whenComplete(() => storageRef.getDownloadURL());
 
       return await snapshot.ref.getDownloadURL();
+    } catch (e) {
+      debugPrint('############ $e');
+      return null;
+    }
+  }
 
+  Future<String?> uploadPostFile({required File file}) async {
+    try {
+      String fileName = basename(file.path);
+
+      Reference storageRef =
+          _firebaseStorage.ref().child('post_images').child(fileName);
+
+      UploadTask storageUploadTask = storageRef.putFile(file);
+
+      TaskSnapshot snapshot = await storageUploadTask
+          .whenComplete(() => storageRef.getDownloadURL());
+
+      return await snapshot.ref.getDownloadURL();
     } catch (e) {
       debugPrint('############ $e');
       return null;
